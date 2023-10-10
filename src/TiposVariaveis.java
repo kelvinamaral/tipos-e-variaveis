ada165e035df070f8ec98b5f4cc59199ffbd3b27
Kelvin <kelvinfamaral@hotmail.com>
2023-10-10 13:12:08 -0300

    Aula Tipos e variaveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        System.out.println("O salário minimo é: " + salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        int numero = 5;
+
+        numero = 10;
+        System.out.println(numero);
+        final double VALOR_DE_PI  = 3.14;
+        System.out.println("O valor de Pi é: " +VALOR_DE_PI);
     }
 }
